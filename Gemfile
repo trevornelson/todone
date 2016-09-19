fb6330884b1864df25efd54dd1a737d0b492e960
Trevor Nelson <trevor.nelson1@gmail.com>
2016-09-18 20:07:35 -0400

    Install and config BarkMQ pub/sub gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'barkmq', github: 'barkbox/barkmq'
 gem 'bower-rails'
 gem 'angular-rails-templates'
 gem 'devise_token_auth'
---
+++ config/initializers/barkmq.rb
@@ -0,0 +1,17 @@
+topic_namespace = [ Rails.env, 'todone' ].join('-')
+queue_name = [ Rails.env, 'todone' ].join('-')
+
+BarkMQ.publisher_config do |c|
+  c.logger = Rails.logger
+  c.topic_namespace = topic_namespace
+  c.queue_name = queue_name
+
+  c.error_handler = BarkMQ::Handlers::DefaultError.new namespace: 'publisher',
+                                                       logger: Rails.logger
+end
+
+BarkMQ.subscriber_config do |c|
+  c.logger = Rails.logger
+  c.topic_namespace = topic_namespace
+  c.queue_name = queue_name
+end
