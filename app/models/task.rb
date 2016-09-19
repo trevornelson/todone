1b2cb09db40ea26620e136c47c3c8f3d463bc738
Trevor Nelson <trevor.nelson1@gmail.com>
2016-09-18 20:43:33 -0400

    Set up Task model as publisher

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,21 @@ class Task < ActiveRecord::Base
 
   before_save :move_to_last_position
 
+  acts_as_publisher on: [ :create ]
+
+  after_publish :publish_task, topic: 'todone',
+                                on: [ :create ],
+                                error: publish_task_error,
+                                complete: Proc.new { puts "complete: " }
+
+  def publish_task
+    self.publish_to_sns('todone')
+  end
+
+  def publish_task_error
+    Rails.logger.error "Publish task failed. error=#{error.inspect}"
+  end
+
   def move_to_last_position
     self.order ||= user.tasks.where(completed: false).where(archived: false).count + 1
   end
@@ -11,5 +26,8 @@ class Task < ActiveRecord::Base
     self.completed = true
     self.order = nil
     self.move_to_last_position
+  end
+
+  def analyze
   end
 end
